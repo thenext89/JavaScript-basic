a42b62c88acf08fb66ddb530c9d5c0bdba8aeffb
thenext89 <gc123580@gmail.com>
2022-07-16 16:51:33 +0700

    su dung bien

--- video3/video3.js
+++ video3/video3.js
@@ -1,4 +1,11 @@
+// khai bao bien;
 let c =5;
 let d =5.9;
-console.log('hello world with javascript')
+let x=5;
+let y = 5;
+let z = 6;
+//khai bao hang so  
+const s =5;// gia tri khong bao h thay doi
+console.log('hello world with javascript',x==y);
+console.log(`test string` ,`x=`,x,`y=`,y,`z=`,z);
 
