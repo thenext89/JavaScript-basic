802079eeeabce21e8e505adbe11b6a63babe4ae3
thenext89 <gc123580@gmail.com>
2022-07-16 17:37:52 +0700

    cac ham can nho cua string

--- video4/video4.js
+++
@@ -1,4 +0,0 @@
-let a = `Tôi là một string/chuỗi`;// Sử dụng dấu gạch chéo để không bị nhầm lẫn giữa dấu nháy đơn và kép
-console.log(a);
-console.log(4+5+'a');
-console.log('a'+5+4);
---
+++ video5/video5.js
@@ -0,0 +1,5 @@
+console.log(`hello world form html`);
+let text=`     abc123    qer`;
+let trim=text.trim();
+console.log(text.length);
+console.log(trim.length)
