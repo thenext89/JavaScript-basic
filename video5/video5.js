f8e7ad84a6d7d9e191ddba913ad208cd023e91f1
thenext89 <gc123580@gmail.com>
2022-07-17 15:58:53 +0700

    kieu so trong js

--- video5/video5.js
+++
@@ -1,5 +0,0 @@
-console.log(`hello world form html`);
-let text=`     abc123    qer`;
-let trim=text.trim();
-console.log(text.length);
-console.log(trim.length)
---
+++ video6/video6.js
@@ -0,0 +1,15 @@
+console.log(`hello world from html`.toUpperCase())
+let a = 5;// khong can khai bao kieu du lieu cua so
+let b = 6;
+let c = 6.9
+let d = '5'
+// let e = Number('5');
+let e = +d // meo convert sang kieu so
+console.log(e,`e: `, typeof e)
+console.log(a + b);
+console.log(a + c);
+console.log(a + d);
+console.log(a * d)
+console.log(a-d);
+console.log(a===e, `a: `,typeof a, `d: `, typeof d);
+// ngoai tru phep cong thi se convert sang kieu so de tinh.
