868518d66c18ed2ac7ed7a7af96a32ed92e0a936
thenext89 <gc123580@gmail.com>
2022-07-17 16:28:41 +0700

    string template/string literals & phan biet let va var

---
+++ video7/video7.js
@@ -0,0 +1,22 @@
+let a=10;
+let b=12;
+console.log(`tong cua a+b=${a+b}`)// template strings/ template literals
+console.log(`gia tri cua a la: ${a}
+gia tri cua b = ${b}`)
+
+
+// let vs var, let bat loi chat. hon var, VD
+function run(){
+    var tiep='tiep';
+    let duc =`duc`;
+    console.log(tiep,duc)
+    {
+        var tiep1=`tiep 1`;
+        let duc1 = 1;
+        console.log(tiep1);
+        console.log(duc1);
+    }
+    console.log(tiep1);// tiep1 chua khai bao nhung van co gia tri
+    console.log(duc1);
+}
+run();
