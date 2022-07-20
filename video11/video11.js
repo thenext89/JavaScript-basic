f508c3b7d5b28e77966d5444c8b8351fb581943a
thenext89 <gc123580@gmail.com>
2022-07-20 18:34:32 +0700

    vong lap for(for loop)

---
+++ video11/video11.js
@@ -0,0 +1,10 @@
+console.log('hello world from html'.toUpperCase());
+// làm quen với vòng lặp for
+let arr=['mu','chelsea','mancty','liverpool']
+for (let i = 0; i < arr.length; i++) {
+    // console.log('check variable i = ',i)
+    console.log(`check value arr[${i}] = `,arr[i])
+}
+
+
+console.log(`check value i: `,i)// i is not defined
