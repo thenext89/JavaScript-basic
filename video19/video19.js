9f14b29a002400260ee6ee3ea33fbd89acc14d99
thenext89 <89thenext@gmail.com>
2022-10-28 13:54:52 +0700

    video 19 phan biet function va method

---
+++ video19/video19.js
@@ -0,0 +1,13 @@
+console.log(`hello world`)
+
+// function sum1(a, b) {
+//     return a + b;
+// }
+// arrows function giống function nhưng ngắn hơn(không cần let)
+let sum = (a, b) => {
+    return a + b;
+}
+console.log(`sum a+b= `, sum(7, 8))
+// function vs method
+// function thuong dung mot minh, khong phu thuoc vao doi tuong hay bat cu thu gi
+// method thuong dc goi ra trong doi tuong/ member of object of class
